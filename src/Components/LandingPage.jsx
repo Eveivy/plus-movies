b3ee97ea3690d9148b3c91e7e98526c8ebe461e1
Isaac Evelyn <evelyn.isaac@cinfores.com>
2023-03-31 05:01:08 +1300

    landing page build

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -3,6 +3,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Header from './Header';
+import NewsCarousel from './NewsCarousel';
+import aboutSectionImg from '../assets/Images/about-img.jpg'
 
 export default function LandingPage() {
     return (
@@ -15,27 +17,156 @@ export default function LandingPage() {
                         <p className='lead'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi recusandae alias ut voluptatibus praesentium,
                             ab, obcaecati veniam, dolorem cumque ex officia saepe consequuntur voluptates exercitationem sed ad! Consectetur, nesciunt atque.</p>
                         <div className='my-3'>
-                            <Button className='btn-hot-pink border-0 px-5 py-3 rounded-pill'>Explore</Button>
+                            <Button className='btn-hot-pink border-0 px-5 py-3 rounded-pill'>Explore Movies</Button>
                         </div>
                     </Col>
                 </Row>
             </Container>
-            <Container fluid className='p-5 text-white' style={{ backgroundColor: 'black' }}>
-                <Row>
-                    <Col xl={12} className="p-3 mb-4">
-                        <h2 className='text-uppercase text-center fw-bolder display-5 text-hot-pink'>Trending movies</h2>
+            <Container fluid className='p-xl-5 text-white' style={{ background: 'linear-gradient(to bottom, #191f3f, black)'}}>
+                <Row className='g-5'>
+                    <Col xl={12} className="p-3">
+                        <h2 className='text-uppercase text-center fw-bolder display-5 text-white'>Trending movies</h2>
                     </Col>
-                    <Col xl={3} className='trending rounded-3 d-flex flex-column align-items-start justify-content-end' style={{}}>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
                             <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
                             <div className='d-flex align-items-start justify-content-between flex-row w-100'>
-                                <div className="me-xl-4 pb-3">
+                                <div className="pb-3">
                                     <span className="d-block">2022</span>
                                     <small className="d-block text-warning">7.9 rating</small>
                                 </div>
-                                <div className="me-xl-4 pb-3">
-                                     
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
                                 </div>
                             </div>
+                        </div>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className='p-xl-5 text-white' style={{ background: 'linear-gradient(to bottom, black, #191f3f)'}}>
+                <Row className='g-5'>
+                    <Col xl={12} className="p-3">
+                        <h2 className='text-uppercase text-center fw-bolder display-5 text-white'>Most rated movies</h2>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xl={3} style={{}}>
+                        <div className='trending rounded-3 d-flex flex-column align-items-start p-2 justify-content-end'>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="pb-3">
+                                 <Button size='sm' className='rounded-pill px-3 btn-light-pink border-0'>View</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className='p-xl-5 pb-xl-0' style={{ background: 'linear-gradient(to bottom, #191f3f, black)'}} >
+                <Row>
+                    <Col xl={12} className="container p-xl-5 ">
+                        <Container className='p-xl-5'>
+                          <NewsCarousel/>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className='p-xl-5 text-white' style={{ background: 'linear-gradient(to bottom, black, #191f3f)'}} >
+                <Row>
+                    <Col xl={6} className="container d-flex flex-column justify-content-center">
+                       <h4 className='display-3'>About</h4> 
+                       <p className='lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum totam consectetur ex, fugiat libero dignissimos animi ipsa.
+                         Cum fugiat sed amet eveniet qui mollitia quo et, accusamus culpa illum praesentium.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda repudiandae sunt ipsa. Atque, mollitia fuga.</p>
+                    </Col>
+                    <Col xl={6} className="container">
+                    <img src={aboutSectionImg} alt="" className='img-fluid rounded-3'/>
                     </Col>
                 </Row>
             </Container>
---
+++ src/Components/NewsCarousel.jsx
@@ -0,0 +1,72 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
+import dPoster from '../assets/Images/divergent.jpg'
+import phPoster from '../assets/Images/purple-hearts.jpg'
+
+export default function NewsCarousel() {
+    return (
+        <Carousel fade>
+            {/* <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={dPoster}
+                    alt="First slide"
+                    style={{height: '650px'}}
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={phPoster}
+                    alt="First slide"
+                    style={{height: '650px'}}
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={dPoster}
+                    alt="First slide"
+                    style={{height: '650px'}}
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item> */}
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 rounded-top"
+                    src={phPoster}
+                    alt="First slide"
+                    style={{height: '600px'}}
+                />
+                <Container className='text-center py-5 text-white rounded-bottom' style={{ background: 'linear-gradient(to bottom, black, #191f3f)'}}>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Container>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 rounded-top"
+                    src={dPoster}
+                    alt="First slide"
+                    style={{height: '600px'}}
+                />
+                <Container className='text-center py-5 text-white rounded-bottom' style={{ background: 'linear-gradient(to bottom, black, #191f3f)'}}>
+                    <h3>Second slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Container>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
