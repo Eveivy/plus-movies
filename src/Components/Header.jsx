de463f29587d06f08dccd1c1a4273102d8c82d6b
Isaac Evelyn <evelyn.isaac@cinfores.com>
2023-03-31 02:01:54 +1300

    landing page build

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,37 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import logo from '../assets/Images/logo.png'
+import 'boxicons'
 
 export default function Header() {
     return (
         <Navbar collapseOnSelect expand="lg">
-            <Container>
-                <Navbar.Brand href="#home" className='text-white'>React-Bootstrap</Navbar.Brand>
+            <Container fluid className='mx-xl-4 d-xl-flex align-items-center'>
+                <Navbar.Brand href="#home" className='text-white'>
+                    <img src={logo} alt="" style={{ width: '70px', height: '55px' }} className="mb-0" />
+                    <span className='d-block font-main' style={{ marginTop: '-13px' }}>Plus Movies</span>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="m-auto">
-                        <Nav.Link href="#features" className='text-white'>Features</Nav.Link>
-                        <Nav.Link href="#pricing" className='text-white'>Pricing</Nav.Link>
-                     
+                        <Nav.Link href="#home" className='text-white px-xl-4'>Home</Nav.Link>
+                        <Nav.Link href="#movies" className='text-white px-xl-4'>Movies</Nav.Link>
+                        <Nav.Link href="#series" className='text-white px-xl-4'>Series</Nav.Link>
+                        <Nav.Link href="#category" className='text-white px-xl-4'>Category</Nav.Link>
+                        <Nav.Link href="#about" className='text-white px-xl-4'>About</Nav.Link>
                     </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets" className='text-white'>More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes" className='text-white'>Dank memes</Nav.Link>
+                    <Nav className='ms-auto'>
+                        <div className='bg-white rounded-3 d-flex align-items-center p-2'>
+                            <span className='mx-2 d-flex align-items-center justify-content-center'>
+                                 <box-icon name='search-alt-2' color="#ff0088" size="25px"></box-icon> 
+                            </span>
+                            <Form as='input' className='search-input fs-6' id='search_input' placeholder='Search'/>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
 }
- 
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,17 +1,44 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import Header from './Header';
 
 export default function LandingPage() {
     return (
-       <>
-       <Container fluid className='hero-section'>
-            <Header/>
-
-       </Container>
-       </>
+        <>
+            <Container fluid className='hero-section'>
+                <Header />
+                <Row className='h-75'>
+                    <Col xl={8} className='d-flex justify-content-center flex-column text-white ms-xl-5 mt-xl-5'>
+                        <h1 className='display-2 fw-bold'>Unlimited movies, <br /> TV shows and more</h1>
+                        <p className='lead'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi recusandae alias ut voluptatibus praesentium,
+                            ab, obcaecati veniam, dolorem cumque ex officia saepe consequuntur voluptates exercitationem sed ad! Consectetur, nesciunt atque.</p>
+                        <div className='my-3'>
+                            <Button className='btn-hot-pink border-0 px-5 py-3 rounded-pill'>Explore</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className='p-5 text-white' style={{ backgroundColor: 'black' }}>
+                <Row>
+                    <Col xl={12} className="p-3 mb-4">
+                        <h2 className='text-uppercase text-center fw-bolder display-5 text-hot-pink'>Trending movies</h2>
+                    </Col>
+                    <Col xl={3} className='trending rounded-3 d-flex flex-column align-items-start justify-content-end' style={{}}>
+                            <h3 className='fs-2 mb-0 pb-0'>Wednesday</h3>
+                            <div className='d-flex align-items-start justify-content-between flex-row w-100'>
+                                <div className="me-xl-4 pb-3">
+                                    <span className="d-block">2022</span>
+                                    <small className="d-block text-warning">7.9 rating</small>
+                                </div>
+                                <div className="me-xl-4 pb-3">
+                                     
+                                </div>
+                            </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
- 
