1004bd03cc54a61f6ab4a432b3d9dcec03eb12a0
Isaac Evelyn <evelyn.isaac@cinfores.com>
2023-04-13 16:14:22 +0100

    API implementations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
+import Movies from './Components/Movies';
+
 
 function App() {
 
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/movies" component={Movies} /> 
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/Components/Movies.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+ 
+
+export default function Movies() {
+    return (
+        <>
+           <div>Movies Here</div>
+        </>
+    );
+}
